ca314c8176bbbaf3e104f6eff8576798991aab7d
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-05 14:18:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from mongo import db
 
 scoredb = db.score
+userchatdb = db.uc
 
 async def get_user_score(user_id: int, chat_id: int):
     get_score = scoredb.find_one({"chat_id": chat_id, "user_id": user_id})
@@ -12,6 +13,17 @@ async def get_user_score(user_id: int, chat_id: int):
 
 async def update_user_score(user_id: int, chat_id: int, score: int):
     try:
-        await scoredb.update_one({"user_id": user_id, "chat_id": chat_id}, {"$set": {"score": score}}, upsert=True)
+        await scoredb.update_one({"chat_id": chat_id, "user_id": user_id}, {"$set": {"score": score}}, upsert=True)
     except:
         pass
+
+async def add_user_in_chat(user_id: int, chat_id: int):
+    already = await userchatdb.find_one({"chat_id": chat_id})
+    user_list = []
+    if already:
+        user_list = already["user_list"]
+        if user_id in user_list:
+            return
+    user_list.append(user_id)
+    await userchatdb.update_one({"chat_id": chat_id}, {"$set": {"user_list": user_list}})
+
