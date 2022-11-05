98dfca1ebdd5975df2e85f66f865e789f325f4e1
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-05 19:04:41 +0530

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,7 @@
+class Strings:
+    START_TEXT = """👋🏻 Welcome {}
+
+💭 This bot will periodically send images to your group with text. The user who writes the word fastest possible will win coins.
+
+🆙 Share your bot profile inline by typing <code>@{}</code> wherever you want!"""
+    
