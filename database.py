96f79a7d48b784099315364233c1b4d81990e605
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-05 14:34:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -14,5 +14,5 @@ async def update_user_score(chat_id: int, user_id: int, points: int):
     get_score = await get_user_score(chat_id, user_id)
     final = get_score + points
     update_set = {"user_info": {str(user_id): {"score": final}}}
-    await score.update_one({"chat_id: chat_id}, {"$set": update_set})
+    await score.update_one({"chat_id": chat_id}, {"$set": update_set}, upsert=True)
 
