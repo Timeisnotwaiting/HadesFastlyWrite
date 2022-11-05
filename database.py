9e4905061a2ec23adc39490d9231415867a96472
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-05 07:50:24 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from mongo import db
 scoredb = db.score
 
 async def get_user_score(user_id: int, chat_id: int):
-    get_score = await scoredb.find_one({"user_id": user_id, "chat_id": chat_id})
+    get_score = scoredb.find_one({"chat_id": chat_id, "user_id": user_id})
     if get_score:
         score = get_score["score"]
         return score
