dedc53c34087f90591b80585e4512bdca57c65c7
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-13 11:16:09 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -7,4 +7,11 @@ db = mongo.HFW
 
 scoredb = db.score
 
-async def 
+async def get_user_score(chat_id: int, user_id: int):
+    x = await scoredb.find_one({"chat_id": chat_id})
+    if x:
+        return x[str(user_id)]["score"]
+    return 0
+
+async def update_user_score(chat_id: int, user_id: int, score: int):
+    return await scoredb.update_one({"chat_id": chat_id}, {"$set": {str(user_id): {"score": score}}}, upsert=True)
